12a14976aa8d2e244eb3f5a86940b7df36acc2ee
yanglikun <yanglikun@jd.com>
2020-08-07 17:07:32 +0800

    如果是cglib的化接口上的注解不起作用，因为这个method是实现类的method

--- src/main/java/io/github/yanglikun/interfaceorimpl/aop/ExceptionAdvice.java
+++ src/main/java/io/github/yanglikun/interfaceorimpl/aop/ExceptionAdvice.java
@@ -1,6 +1,7 @@
 package io.github.yanglikun.interfaceorimpl.aop;
 
 import org.springframework.aop.ThrowsAdvice;
+import org.springframework.core.annotation.AnnotationUtils;
 
 import java.lang.reflect.Method;
 
@@ -10,7 +11,14 @@ import java.lang.reflect.Method;
 public class ExceptionAdvice implements ThrowsAdvice {
 
     public void afterThrowing(Method method, Object[] args, Object target, Throwable subclass) throws Throwable {
+        //不会查找继承体系
         boolean dontNeedAdvice = method.isAnnotationPresent(NoExceptionAdiveAnno.class);
+        //会查找继承体系
+        NoExceptionAdiveAnno annotation = AnnotationUtils.findAnnotation(method, NoExceptionAdiveAnno.class);
+
+        System.out.println("通过AnnotationUtils查找:" + (annotation != null));
+        System.out.println("通过method查找:" + dontNeedAdvice);//如果用cg
+
         if (dontNeedAdvice) {
             //有注解什么也不做
         } else {
--- src/test/java/interfaceorimpl/TestCode.java
+++ src/test/java/interfaceorimpl/TestCode.java
@@ -2,6 +2,7 @@ package interfaceorimpl;
 
 import io.github.yanglikun.interfaceorimpl.service.OrderServiceI;
 import io.github.yanglikun.interfaceorimpl.service.impl.MainService;
+import io.github.yanglikun.interfaceorimpl.service.impl.OrderServiceImpl;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -16,8 +17,20 @@ public class TestCode {
         OrderServiceI orderServiceI = ctx.getBean(OrderServiceI.class);
         System.out.println("OrderServiceI:" + orderServiceI.getClass());
         orderServiceI.save();
-
     }
+
+    /**
+     * 这个会报错，因为proxy不是Impl的实例
+     * com.sun.proxy.$Proxy10 cannot be cast to io.github.yanglikun.interfaceorimpl.service.impl.OrderServiceImpl
+     */
+    @Test
+    public void testOrderServiceImpl() {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("interfaceorimpl/spring-aop-interfaceorimpl.xml");
+        OrderServiceImpl orderServiceImpl = (OrderServiceImpl) ctx.getBean("orderService");
+        System.out.println("orderServiceImpl:" + orderServiceImpl.getClass());
+        orderServiceImpl.save();
+    }
+
 
     @Test
     public void testMainService() {
